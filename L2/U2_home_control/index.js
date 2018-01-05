714561833922e2520c624c5feb35a8a32b373910
bernard <bernard@alphacamp.tw>
2018-01-05 14:20:10 +0800

    After Slide restructure but havent re-organize code yet

---
+++ L2/U2_home_control/index.js
@@ -0,0 +1,25 @@
+var state = "on";
+
+    function toggleLight() {
+      var img = document.getElementById('image');
+      if ( state == "on" ) {
+        state = "off";
+        image.src = "img/off.jpg";
+      } else {
+        state = "on";
+        image.src = "img/on.jpg";
+      }
+    }
+
+    var timeLeft = 5;
+
+    function countDownTimer() {
+      timeLeft = timeLeft - 1;
+      if (timeLeft <= 0) {
+            document.getElementById("image").src = "img/off.jpg";
+            clearInterval(setting);
+      }
+      document.getElementById("timer").innerHTML = timeLeft;
+    } 
+
+    var setting = setInterval(countDownTimer, 1000);
