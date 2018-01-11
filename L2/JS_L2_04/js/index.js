6736fdf1ea66fcaf1036af944d1ce5620f14dd6a
Arch <bernard@alphacamp.tw>
2018-01-11 23:53:31 +0800

    Fix JS_L2_04 naming and indentation

--- L2/JS_L2_04/js/index.js
+++ L2/JS_L2_04/js/index.js
@@ -1,26 +1,26 @@
 "use strict";
 var state = "on";
 
-    function toggleLight() {
-      var img = document.getElementById('image');
-      if ( state == "on" ) {
-        state = "off";
-        img.src = "img/off.jpg";
-      } else {
-        state = "on";
-        img.src = "img/on.jpg";
-      }
-    }
+function toggleLight() {
+  var image = document.getElementById('image');
+  if ( state == "on" ) {
+    state = "off";
+    image.src = "img/off.jpg";
+  } else {
+    state = "on";
+    image.src = "img/on.jpg";
+  }
+}
 
-    var timeLeft = 5;
+var timeLeft = 5;
 
-    function countDownTimer() {
-      timeLeft = timeLeft - 1;
-      if (timeLeft <= 0) {
-            document.getElementById("image").src = "img/off.jpg";
-            clearInterval(setting);
-      }
-      document.getElementById("timer").innerHTML = timeLeft;
-    } 
+function countDownTimer() {
+  timeLeft = timeLeft - 1;
+  if (timeLeft <= 0) {
+    document.getElementById("image").src = "img/off.jpg";
+    clearInterval(setting);
+  }
+  document.getElementById("timer").innerHTML = timeLeft;
+} 
 
-    var setting = setInterval(countDownTimer, 1000);
+var setting = setInterval(countDownTimer, 1000);
